a7ddebb7042c5d55f586e18f156f592dc62af6e4
Scatmandu <mcclur05@nsuok.edu>
2020-12-15 23:06:11 +0000

    changed if not to if also returned original list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,8 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0 or idx > length:
         return my_list
-    if not my_list:
-        return
+    if length == 0:
+        return my_list
     else:
         for x in range(length):
             if x == idx:

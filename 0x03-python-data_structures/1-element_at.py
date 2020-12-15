9fb9a749da5736fba29a240e9449c915aa0f2e2d
Scatmandu <mcclur05@nsuok.edu>
2020-12-15 21:42:17 +0000

    fixed sloppy logic in file 1 and added file 2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     length = len(my_list)
-    for x in range(length):
-        if idx < 0 or idx > length:
-            return
-        elif x == idx:
-            return my_list[x]
+    if idx < 0 or idx > length:
+        return
+    else:
+        for x in range(length):
+            if x == idx:
+                return my_list[x]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
-    for x in range(length):
-        if idx < 0 or idx > length:
-            return my_list
-        elif x == idx:
-            my_list[idx] = element
-            return my_list
+    if idx < 0 or idx > length:
+        return my_list
+    else:
+        for x in range(length):
+            if x == idx:
+                my_list[idx] = element
+                return my_list

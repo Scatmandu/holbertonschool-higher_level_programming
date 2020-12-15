7fc3446d0e7d133f9c37ec34eb095a6b7ed3548d
Scatmandu <mcclur05@nsuok.edu>
2020-12-15 23:08:34 +0000

    accounted for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list) - 1
+    if len(my_list) == 0:
+        print("0")
     while length >= 0:
         print("{:d}".format(my_list[length]))
         length = length - 1

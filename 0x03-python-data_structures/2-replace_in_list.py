9741f5326af40e375eee6e9390dfc083a7769bc6
Scatmandu <mcclur05@nsuok.edu>
2020-12-15 23:03:34 +0000

    accounted for empty list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,8 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0 or idx > length:
         return my_list
+    if not my_list:
+        return
     else:
         for x in range(length):
             if x == idx:

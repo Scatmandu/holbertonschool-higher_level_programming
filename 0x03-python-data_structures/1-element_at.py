71d4425004dbd768522743bb942fadd6a35cc8cc
Scatmandu <63303265+Scatmandu@users.noreply.github.com>
2021-04-17 19:02:27 -0500

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0 or idx > length:
-        return
-    else:
-        for x in range(length):
-            if x == idx:
-                return my_list[x]

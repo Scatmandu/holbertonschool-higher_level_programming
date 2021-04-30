c0a88f3edc8aa3d21052cb9a2e50080510adc0b0
Scatmandu <mcclur05@nsuok.edu>
2021-04-30 16:02:44 +0000

    removed replace()

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    remove_c = ['c', 'C']
-    for i in remove_c:
-        my_string = my_string.replace(i, '')
-    return(my_string)
+    return(my_string.translate({ord(i): None for i in 'cC'}))

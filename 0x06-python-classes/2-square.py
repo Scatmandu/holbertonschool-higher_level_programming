db2d6467b8f79aa99bba3a3a65734563a51b4900
Scatmandu <mcclur05@nsuok.edu>
2021-05-04 19:27:58 +0000

    added file 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creates class square with a size and makes sure size is an int and >= 0"""
+
+
+class Square:
+    """
+    square class that holds size
+
+    Attributes:
+        __size (integer): the size of the square instance
+
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a square of size size
+
+        Args:
+            size: the size of the new square instance
+
+        """
+        if isinstance(size, int) is True:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")

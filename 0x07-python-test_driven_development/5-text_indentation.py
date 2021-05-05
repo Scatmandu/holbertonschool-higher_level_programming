a2bdc744b60ed01475cff2d98aa20976e5d6bd93
Scatmandu <mcclur05@nsuok.edu>
2021-05-05 21:42:29 +0000

    added files 4 and 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""inserts two newlines after each . ? and :"""
+
+def text_indentation(text):
+    """inserts two newlines after each . ? and :"""
+
+    skip = False
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for x in text:
+        if skip is True:
+            skip = False
+            continue
+        if x == '.':
+            print("{}\n".format(x))
+            skip = True
+        elif x == '?':
+            print("{}\n".format(x))
+            skip = True
+        elif x == ':':
+            print("{}\n".format(x))
+            skip = True
+        else:
+            print(x, end="")

9e93cec6fda44eecf2003b3e74c0a5991bfea674
Scatmandu <mcclur05@nsuok.edu>
2021-05-05 20:25:53 +0000

    added both files for 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function that prints a first and last name"""
+
+def say_my_name(first_name, last_name=""):
+    """function that prints a first and last name"""
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    if first_name is None:
+        raise TypeError("first_name must be a string")
+    if last_name is None:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
+    

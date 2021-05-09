f891ea6438232227c0db7fdf82a2198c51f01c1c
Scatmandu <mcclur05@nsuok.edu>
2021-05-09 17:45:36 +0000

    errant dots removed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,29 +9,29 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def max_at_end(self):
-        self.assertEqual.(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def max_in_middle(self):
-        self.assertEqual.(max_integer([1, 9, 2]), 9)
+        self.assertEqual(max_integer([1, 9, 2]), 9)
 
     def max_at_beginning(self):
-        self.assertEqual.(max_integer([9, 3, 1]), 9)
+        self.assertEqual(max_integer([9, 3, 1]), 9)
 
     def test_none(self):
-        self.assertEqual.(max_integer([]), None)
+        self.assertEqual(max_integer([]), None)
 
     def test_one_negative(self):
-        self.assertEqual.(max_integer([1, -1, 3]), 3)
+        self.assertEqual(max_integer([1, -1, 3]), 3)
 
     def test_all_negative(self):
-        self.assertEqual.(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
     def test_one(self):
-        self.assertEqual.(max_integer([7]), 7)
+        self.assertEqual(max_integer([7]), 7)
 
     def test_unordered(self):
-        self.assertEqual.(max_integer([4, 1, 7, 9]), 9)
+        self.assertEqual(max_integer([4, 1, 7, 9]), 9)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

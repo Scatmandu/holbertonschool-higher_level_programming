26c54e86727fff0835d78af524336777e57bd1d3
Scatmandu <mcclur05@nsuok.edu>
2021-05-09 17:33:35 +0000

    added more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,13 +8,22 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMax(unittest.TestCase):
 
-    def test_ordered(self):
+    def max_at_end(self):
         self.assertEqual.(max_integer([1, 2, 3, 4]), 4)
+
+    def max_in_middle(self):
+        self.assertEqual.(max_integer([1, 9, 2]), 9)
+
+    def max_at_beginning(self):
+        self.assertEqual.(max_integer([9, 3, 1]), 9)
 
     def test_none(self):
         self.assertEqual.(max_integer([]), None)
 
-    def test_negative(self):
+    def test_one_negative(self):
+        self.assertEqual.(max_integer([1, -1, 3]), 3)
+
+    def test_all_negative(self):
         self.assertEqual.(max_integer([-1, -2, -3]), -1)
 
     def test_one(self):

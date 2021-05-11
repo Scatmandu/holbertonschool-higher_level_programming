541341339792b18ef30ce55e42dd37c6ac433c6c
Scatmandu <mcclur05@nsuok.edu>
2021-05-11 22:23:38 +0000

    added file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class BaseGeometry with method for area"""
+
+
+class BaseGeometry:
+    """class BaseGeometry with method for area"""
+    def area(self):
+        """empty method"""
+        raise Exception("area() is not implemented")

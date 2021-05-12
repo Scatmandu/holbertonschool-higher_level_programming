82de9e781fec4c37789ec70d65c2f8767879f7e5
Scatmandu <mcclur05@nsuok.edu>
2021-05-12 21:52:41 +0000

    added file 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns dict descripton for a JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """read it up there lol it's too long for this line"""
+
+    return obj.__dict__

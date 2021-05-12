c984179b69812493f9bd7c0e50a9f46870aa947a
Scatmandu <mcclur05@nsuok.edu>
2021-05-12 01:52:41 +0000

    added file 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""import classes BaseGeometry and Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """calculates the area of a square"""
+
+        return self.__size * self.__size

d7097f69111c347fcdc9377feb118861defa734f
Scatmandu <mcclur05@nsuok.edu>
2021-05-12 20:37:13 +0000

    added file 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""writes a string to a txt file and returns number of characters"""
+
+
+def write_file(filename="", text=""):
+    """writes string to txtfile and returns number of chars"""
+
+    count = 0
+
+    for char in text:
+        count += 1
+    return count
+
+    with open(filename, "w") as f:
+        f.write(text)

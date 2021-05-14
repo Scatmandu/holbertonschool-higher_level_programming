c0f773798374d3277b24ab2ca68d061e3d937633
Scatmandu <mcclur05@nsuok.edu>
2021-05-14 23:01:40 +0000

    added base.py and __init__.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""creating the class Base"""
+
+
+class Base:
+    """class Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes Base instance"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

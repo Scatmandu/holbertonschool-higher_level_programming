92f872b966438eea81962f853d0e309b5e052b93
Scatmandu <mcclur05@nsuok.edu>
2021-05-16 19:23:19 +0000

    added to_dictionary method to rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,6 +63,17 @@ class Rectangle(Base):
             if "y" in kwargs:
                 self.y = kwargs["y"]
 
+    def to_dictionary(self):
+        """returns dictionary representation of rectangle"""
+        rec_dict = {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+        return rec_dict
+
     @property
     def width(self):
         """returns private instance attribute width"""

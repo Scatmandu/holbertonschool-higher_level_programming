94e62de8581eb1c45a1abea1b27a25c9656a11e7
Scatmandu <mcclur05@nsuok.edu>
2021-05-16 18:52:18 +0000

    added getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -59,61 +59,12 @@ class Square(Rectangle):
                 self.y = kwargs["y"]
 
     @property
-    def width(self):
-        """returns private instance attribute width"""
-        return self.__width
+    def size(self):
+        """getter for size"""
+        return self.width
 
-    @property
-    def height(self):
-        """returns private instance attribute height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """returns private instance attribute x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """returns private instance attribute y"""
-        return self.__y
-
-    @width.setter
-    def width(self, width):
-        """sets width"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = width
-
-    @height.setter
-    def height(self, height):
-        """sets height"""
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = height
-
-    @x.setter
-    def x(self, x):
-        """sets x"""
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        elif x < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = x
-
-    @y.setter
-    def y(self, y):
-        """sets y"""
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        elif y < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = y
+    @size.setter
+    def size(self, size):
+        """setter for size"""
+        self.width = size
+        self.height = size

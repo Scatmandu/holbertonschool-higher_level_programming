aba257a3c5fceb1a5592642840e7dfc4a1e3c94d
Scatmandu <mcclur05@nsuok.edu>
2021-05-18 02:11:27 +0000

    added last two methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,31 @@ class Base:
                 json_rep.append(cls.to_dictionary(objects))
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(json_rep))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with attributes set"""
+        dummy = None
+        if cls.__name__ is "Rectangle":
+            dummy = cls(1, 2, 0, 0, None)
+        elif cls.__name__ is "Square":
+            dummy = cls(1, 0, 0, None)
+        cls.update(dummy, **dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns list of instances"""
+        instance_list = []
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, mode="r") as f:
+                contents = f.read()
+                if contents == "[]":
+                    return(instance_list)
+                json_string = (cls.from_json_string(contents))
+                for instance in json_string:
+                    instance_list.append(cls.create(**instance))
+                return(instannce_list)
+        except FileNotFoundError:
+            return (instance_list)

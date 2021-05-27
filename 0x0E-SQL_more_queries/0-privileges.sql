bd79c88193748782c316897bf31eea32e22542ed
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 23:00:52 +0000

    added IF EXISTS

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- shows privileges of two users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS IF EXISTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS IF EXISTS FOR 'user_0d_2'@'localhost';

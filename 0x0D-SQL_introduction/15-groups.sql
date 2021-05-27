a4f6ae2f0ba0acb43023dd562efe85a5fb44df7d
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 00:18:47 +0000

    simple as a missing comma?

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of scores with same value sorted by number descending
-SELECT COUNT(score) AS number FROM second_table ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table ORDER BY number DESC;

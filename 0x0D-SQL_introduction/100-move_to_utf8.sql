96f906f6a7568383481e5048409aecda65682b1e
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 00:38:17 +0000

    added 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,5 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 --converts table to UTF-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- converts name to UTF-8
+ALTER TABLE first_table MODIFY COLUMN name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

4f1bacc548ef3055db5b82266b465e35d5abed33
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 23:24:49 +0000

    added GRANT USAGE

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grants usage to user
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost'
 -- grants select privileges to user
 GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

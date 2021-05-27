992246c07b54492161bfe95664535930282bfbbc
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 23:59:47 +0000

    tried adding use statement to 6 and 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates database with table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );

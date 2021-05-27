a280ac9254456cecbb352da03c7698fa24f74a52
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 23:22:35 +0000

    added file 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grants select privileges to user
+GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

afabeb04850ae644338fdcd2aba6f099db151532
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 23:13:22 +0000

    fixed typo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a new user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- grants them all privileges
-GRANT ALL PRIVILEGES ON "." TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

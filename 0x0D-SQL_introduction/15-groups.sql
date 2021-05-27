dee3846d201ce87b8bdb825d6475e7131db15f4d
Scatmandu <mcclur05@nsuok.edu>
2021-05-27 00:16:13 +0000

    removed redundant score from select

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of scores with same value sorted by number descending
-SELECT score COUNT(score) AS number FROM second_table ORDER BY number DESC;
+SELECT COUNT(score) AS number FROM second_table ORDER BY number DESC;

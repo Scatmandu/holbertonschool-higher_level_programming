6620e3a2f4e50e09dfd2ac932f676d85ad173670
Scatmandu <mcclur05@nsuok.edu>
2021-05-28 00:00:45 +0000

    fixed syntax for 6 and 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- create database and table with id state id and name
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS cities(
 	id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
 	state_id NOT NULL FOREIGN KEY REFERENCES states(id),
 	name VARCHAR(256) NOT NULL

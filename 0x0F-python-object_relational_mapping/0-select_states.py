7c0943e7d3bce755fae81f8967aa591549c0a875
Scatmandu <mcclur05@nsuok.edu>
2021-05-31 19:10:35 +0000

    added file 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""lists states from database"""
+
+
+import MySQLdb
+from sys import argv
+
+
+db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states")
+result = cursor.fetchall()
+for state in result:
+    print(state)

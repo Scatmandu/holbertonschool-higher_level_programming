e6383600a5337a37088410b2fd4bed8c6dbaeb82
Scatmandu <mcclur05@nsuok.edu>
2021-06-03 04:27:49 +0000

    added 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+const sum = a + b;
+if (isNaN(a) || isNaN(b)) {
+  console.log(NaN);
+} else {
+  console.log(sum);
+}

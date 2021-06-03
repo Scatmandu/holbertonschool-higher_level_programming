bf7f2881ce68f35cb280bf1aeaacd89203aafa91
Scatmandu <mcclur05@nsuok.edu>
2021-06-03 04:42:07 +0000

    added 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// computes/prints a factorial
+const numby = parseInt(process.argv[2]);
+function myFactorial (numby) {
+  if (numby === 0) {
+    return 1;
+  } else if (numby < 0) {
+    return 0;
+  } else {
+    return numby * myFactorial(numby - 1);
+  }
+}
+console.log(myFactorial(numby));

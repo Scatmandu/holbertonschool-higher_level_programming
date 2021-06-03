d454e03d27da527a2b0b6f8d55a2d1b1606076dc
Scatmandu <mcclur05@nsuok.edu>
2021-06-03 03:23:25 +0000

    added 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// creates an array of strings and prints them
+let i;
+const newArray = [];
+newArray[0] = 'C is fun';
+newArray[1] = 'Python is cool';
+newArray[2] = 'JavaScript is amazing';
+for (i = 0; i <= 2; i++) {
+  console.log(newArray[i]);
+}

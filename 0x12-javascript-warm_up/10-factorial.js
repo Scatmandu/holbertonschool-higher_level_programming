0757c244c3624507edfa01026a92051bad862d60
Scatmandu <mcclur05@nsuok.edu>
2021-06-03 04:43:47 +0000

    forgot to account for no arg

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // computes/prints a factorial
 const numby = parseInt(process.argv[2]);
 function myFactorial (numby) {
-  if (numby === 0) {
+  if (numby === 0 || isNaN(numby)) {
     return 1;
   } else if (numby < 0) {
     return 0;

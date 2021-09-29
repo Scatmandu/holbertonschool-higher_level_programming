cd952b4f724f39dba31cebcaeab62d836c276d18
Scatmandu <mcclur05@nsuok.edu>
2021-09-29 19:20:22 +0000

    fixed colon/period based on argv length

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     argc = len(argv)
     if argc != 2:
-        print("{:d} arguments.".format(argc - 1))
+        if argc == 1:
+            print("0 arguments.")
+        else:
+            print("{:d} arguments:".format(argc - 1))
     else:
         print("1 argument:")
     for i in range(1, argc):

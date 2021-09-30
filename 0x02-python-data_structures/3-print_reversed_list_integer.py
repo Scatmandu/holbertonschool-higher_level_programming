2baf9993096139e6042089ee1c00c438533f366e
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 17:50:07 +0000

    forgot to use str.format() to print

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     reverse_list = my_list[::-1]
     for x in range(len(reverse_list)):
-        print(reverse_list[x])
+        print("{}".format(reverse_list[x]))

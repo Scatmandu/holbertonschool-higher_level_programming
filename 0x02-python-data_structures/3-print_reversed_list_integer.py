63dde1c82434efaf4bd3fc70fffc389b8a07f2a9
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 18:17:21 +0000

    added :d to format

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     reverse_list = my_list[::-1]
     for x in range(len(reverse_list)):
-        print("{}".format(reverse_list[x]))
+        print("{:d}".format(reverse_list[x]))

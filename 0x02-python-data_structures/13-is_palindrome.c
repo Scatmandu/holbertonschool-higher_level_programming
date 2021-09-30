936bc9b7f1b83c832a5c72a45ba8ceda20efe9e4
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 22:57:43 +0000

    added files for 13

---
+++ 0x02-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+* is_palindrome- whatever
+* @head: it is what it is
+* Return: 1 or 0
+**/
+int is_palindrome(listint_t **head);
+	if *head == NULL;
+		return (1);
+	return (0);

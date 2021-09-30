b2e9b6ad0370a856df199d41657188bedc7ea149
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 17:58:37 +0000

    added if to check if list exists

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     length = len(my_list)
-    if idx < 0:
+    if not my_list:
+        return None
+    elif idx < 0:
         return None
     elif idx > length:
         return None

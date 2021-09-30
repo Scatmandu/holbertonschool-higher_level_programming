dee7aa2ee4b66770b849b825be6d4f3228a8440c
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 20:34:26 +0000

    added file 7

---
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        new_tup_a = (0, 0)
+    if len(tuple_a) == 1:
+        new_tup_a = (tuple_a[0], 0)
+    if len(tuple_a) >= 2:
+        new_tup_a = (tuple_a[0], tuple_a[1])
+    if len(tuple_b) == 0:
+        new_tup_b = (0, 0)
+    if len(tuple_b) == 1:
+        new_tup_b = (tuple_b[0], 0)
+    if len(tuple_b) >= 2:
+        new_tup_b = (tuple_b[0], tuple_b[1])
+    return tuple(map(lambda x, y: x + y, new_tup_a, new_tup_b))

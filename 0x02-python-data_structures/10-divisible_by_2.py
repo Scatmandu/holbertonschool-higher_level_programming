5d7d1a9dcbab0096f7d866737378734d931ebf39
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 21:27:10 +0000

    added file 10

---
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    new_list = list(my_list)
+    for x in my_list:
+        if x % 2 == 0:
+            new_list[x] = True
+        else:
+            new_list[x] = False
+    return new_list

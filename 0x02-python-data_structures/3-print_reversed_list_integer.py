fd5c0020a5cfa5834cb649995ff01263e3182172
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 20:28:36 +0000

    removed new list variable

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reverse_list = my_list[::-1]
-    if not my_list:
-        return None
-    for x in range(len(reverse_list)):
-        print("{:d}".format(reverse_list[x]))
+    if my_list:
+        for x in my_list[::-1]:
+            print("{:d}".format(x))

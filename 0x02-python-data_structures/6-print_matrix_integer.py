f69f1ab883e2b445102f6f89451fdb1b926cfc38
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 18:22:00 +0000

    added :d to format

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -5,8 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         for value in row:
             i += 1
             if i == len(row):
-                print("{}".format(value), end='')
+                print("{:d}".format(value), end='')
                 i = 0
             else:
-                print("{}".format(value), end=' ')
+                print("{:d}".format(value), end=' ')
         print()

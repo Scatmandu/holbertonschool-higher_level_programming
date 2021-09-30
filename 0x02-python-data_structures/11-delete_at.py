665a9857b1f7dcdd2283eb212c8adbec19e6f0b3
Scatmandu <mcclur05@nsuok.edu>
2021-09-30 19:11:15 +0000

    hail mary fix

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -2,7 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list):
         return my_list
-    del my_list[idx]
-    return my_list
+    else:
+        del my_list[idx]
+        return my_list

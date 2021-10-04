63ba222a278a42e86bb19ea1b8df800885e39261
Scatmandu <mcclur05@nsuok.edu>
2021-10-04 19:47:18 +0000

    removed string rename variable and line broke the line that was too long

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    s = '' + roman_string
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
-    if type(s) != str or s is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
-    for i in range(len(s)):
-        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+    for i in range(len(roman_string)):
+        if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
+            int_val += rom_val[roman_string[i]] - 2 * \
+                rom_val[roman_string[i - 1]]
         else:
-            int_val += rom_val[s[i]]
+            int_val += rom_val[roman_string[i]]
     return int_val

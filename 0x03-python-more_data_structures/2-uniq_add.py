0ee03704952bd5ebf2f64411acb83ee8b35299e8
Scatmandu <mcclur05@nsuok.edu>
2021-10-04 17:37:31 +0000

    added file 2

---
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    numby_store = 0
+    for x in my_list:
+        if x not in new_list:
+            new_list.append(x)
+    for y in new_list:
+        numby_store += y
+    return numby_store

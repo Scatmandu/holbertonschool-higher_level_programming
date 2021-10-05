3be582efa169ca03482aacee876488f647b9796f
Scatmandu <mcclur05@nsuok.edu>
2021-10-05 19:07:40 +0000

    added file 4

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""create class name Square"""
+
+
+class Square:
+    """the square class"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end='')
+            print()

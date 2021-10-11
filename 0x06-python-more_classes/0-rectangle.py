239352a361b6f46c865d7dac8643b3b33250d6d4
Scatmandu <mcclur05@nsuok.edu>
2021-10-11 17:44:47 +0000

    added type and value checking to init function

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,8 +6,18 @@ class Rectangle:
     """class Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
     @property
     def width(self):

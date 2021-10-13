a867cb3b013355cbaf74055778b162de3f4214a6
Scatmandu <mcclur05@nsuok.edu>
2021-10-13 20:46:13 +0000

    added file 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns True if object is an instance of class otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """does what I typed up there"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False

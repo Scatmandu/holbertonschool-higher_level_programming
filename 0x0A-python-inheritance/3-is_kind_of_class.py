9a480abb16a3780f69d517a3c0f3dff3d5de9e43
Scatmandu <mcclur05@nsuok.edu>
2021-10-13 20:50:34 +0000

    added file 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""checks if an object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """does what I typed up there"""
+    return isinstance(obj, a_class)

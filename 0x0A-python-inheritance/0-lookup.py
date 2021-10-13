754582f38b1ec34cad9c9df7f277c80dad910882
Scatmandu <mcclur05@nsuok.edu>
2021-10-13 04:12:21 +0000

    added file 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns list of methods and attributes of an object"""
+
+
+def lookup(obj):
+    """returns list of methods and attributes of an object"""
+    return dir(obj)

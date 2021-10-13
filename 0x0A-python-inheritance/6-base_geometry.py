adfb8593e015e443ede20d24239306a1579399e7
Scatmandu <mcclur05@nsuok.edu>
2021-10-13 21:32:34 +0000

    added file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""adds public instance method area that does nothing atm"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """it's up there"""
+        raise Exception("area() is not implemented")

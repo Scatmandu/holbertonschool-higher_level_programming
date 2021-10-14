982f1b69b7b7e07297238d6bb090c75992be06d1
Scatmandu <mcclur05@nsuok.edu>
2021-10-14 14:42:32 +0000

    fixed the thing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """it's up there"""
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) is True and type(obj) is not a_class:
+        return True
+    else:
+        return False

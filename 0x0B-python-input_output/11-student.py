07b2e028b8096048cbab27162a0730569cabfa5d
Scatmandu <mcclur05@nsuok.edu>
2021-10-22 02:51:35 +0000

    added file 7, 10, and 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        strang = {}
+
+        if attrs is not None:
+            for key in attrs:
+                if key in self.__dict__:
+                    strang[key] = self.__dict__[key]
+            return strang
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+        for x in json:
+            setattr(self, x, json[x])

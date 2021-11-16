db15100746fa49b29d4d56bb7c68e6fe049b9ed5
Scatmandu <mcclur05@nsuok.edu>
2021-11-16 14:50:20 -0600

    used wrong password

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
 db = MySQLdb.connect(host="localhost",
                      user=user,
-                     passwd="",
+                     passwd=password,
                      db=database,
                      port=3306)
 

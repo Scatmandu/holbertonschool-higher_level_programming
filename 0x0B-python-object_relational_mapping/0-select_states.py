90eae8e6e19b35db82b96f6898f89c9ca1127a6e
Scatmandu <mcclur05@nsuok.edu>
2021-11-16 15:09:23 -0600

    fixed indentation

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""lists all states from the database hbtn_0e_0_usa:"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
 
 import MySQLdb
@@ -11,17 +11,17 @@ if __name__ == "__main__":
     password = argv[2]
     database = argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     user=user,
-                     passwd=password,
-                     db=database,
-                     port=3306)
+    db = MySQLdb.connect(host="localhost",
+                         user=user,
+                         passwd=password,
+                         db=database,
+                         port=3306)
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

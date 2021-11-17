e7b07b81be19b0c9e81749354336f2e565e43c5f
Scatmandu <mcclur05@nsuok.edu>
2021-11-17 15:44:27 -0600

    fetched all states before displaying result (disallowed)

--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,10 +14,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        if instance is not None:
-            print("{}: {}".format(instance.id, instance.name))
-            break
-        else:
-            print('Nothing')
+    instance = session.query(State).first()
+    if instance is not None:
+        print("{}: {}".format(instance.id, instance.name))
+    else:
+        print('Nothing')
     session.close()

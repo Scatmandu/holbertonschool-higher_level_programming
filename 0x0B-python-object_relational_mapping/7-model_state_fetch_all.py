4f5e16e93c75227f16fd198a87c5a5eb51014f1e
Scatmandu <mcclur05@nsuok.edu>
2021-11-17 15:30:14 -0600

    fixded print formatting

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """lists all State objects from the database hbtn_0e_6_usa"""
-import sys
+
+
+from sys import argv
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
@@ -8,10 +10,10 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name)
+        print("{}: {}".format(instance.id, instance.name))
     session.close()

3bd6413b33c36938d2a2af4b42365b390c72c9c1
Scatmandu <mcclur05@nsuok.edu>
2021-12-01 14:41:26 -0600

    added file 7

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes in a URL, sends request to URL and displays body of the response"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    request = requests.get(argv[1])
+
+    if request.status_code >= 400:
+        print("Error code: {}".format(status_code))
+    else:
+        print(request.text)

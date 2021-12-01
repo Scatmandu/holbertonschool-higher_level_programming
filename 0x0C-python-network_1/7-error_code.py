b92d3c3683214d754b97a587ad886434d6dc6036
Scatmandu <mcclur05@nsuok.edu>
2021-12-01 14:45:38 -0600

    chmod and fixed status_code syntax line 11

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     request = requests.get(argv[1])
 
     if request.status_code >= 400:
-        print("Error code: {}".format(status_code))
+        print("Error code: {}".format(request.status_code))
     else:
         print(request.text)

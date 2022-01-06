3872ef11a28a04190b1a154598bf9846aa1a40f8
Scatmandu <mcclur05@nsuok.edu>
2022-01-06 14:55:40 -0600

    forgot to account for no args or args.length = 1

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,15 @@
 
 const array = [];
 
-for (let i = 2, j = 0; i < process.argv.length; i++, j++) {
-  array[j] = process.argv[i];
-  array.sort();
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(0);
+} else {
+  for (let i = 2, j = 0; i < process.argv.length; i++, j++) {
+    array[j] = process.argv[i];
+    array.sort();
+  }
+  const arrayIndex = array.length - 2;
+  console.log(array[arrayIndex]);
 }
-const arrayIndex = array.length - 2;
-console.log(array[arrayIndex]);

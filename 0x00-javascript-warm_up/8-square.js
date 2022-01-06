a6d00039c30da6b478d9bd46a86c736ff81a512b
Scatmandu <mcclur05@nsuok.edu>
2022-01-06 13:42:13 -0600

    added file 8

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const numbie = parseInt(process.argv[2]);
+let row = '';
+
+if (numbie) {
+  for (let i = 0; i < numbie; i++) {
+    for (let j = 0; j < numbie; j++) {
+      row += 'X';
+    }
+    console.log(row);
+    row = '';
+  }
+}

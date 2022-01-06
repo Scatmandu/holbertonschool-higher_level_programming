e4e92bfe29a4424ab700d8e7ca0924a12e31c053
Scatmandu <mcclur05@nsuok.edu>
2022-01-06 14:06:37 -0600

    added file 10

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (x) {
+  if (x === 0 || isNaN(x)) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+const numbie = parseInt(process.argv[2]);
+console.log(factorial(numbie));

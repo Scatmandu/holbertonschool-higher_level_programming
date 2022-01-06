5566a4ca6a13a3ce52fe7a370a6e5bb8cdd362d5
Scatmandu <mcclur05@nsuok.edu>
2022-01-06 13:18:47 -0600

    fixed typo in else statement output

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const numbie = parseInt(process.argv[2]);
+
 if (numbie) {
-  console.log('My number is: ' + numbie);
+  console.log('My number: ' + numbie);
 } else {
   console.log('Not a number');
 }

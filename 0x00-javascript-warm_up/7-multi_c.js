97d084bb05d617b78f0bbb70cf8d242bbddc77f8
Scatmandu <mcclur05@nsuok.edu>
2022-01-06 13:23:04 -0600

    added file 7

---
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const strang = 'C is fun';
+const numbie = parseInt(process.argv[2]);
+
+if (Number.isInteger(numbie)) {
+  for (let i = 0; i < numbie; i++) {
+    console.log(strang);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}

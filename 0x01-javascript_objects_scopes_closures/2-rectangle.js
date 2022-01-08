e5a2a84cfb6a44f08b278557f1b9716c5a7218a7
Scatmandu <mcclur05@nsuok.edu>
2022-01-07 19:43:17 -0600

    fixed if statement

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (width, height) {
-    if (this.width > 0 && this.height > 0) {
+    if (width > 0 && height > 0) {
       this.width = width;
       this.height = height;
     }

1ca42c5062895a2ffae0589d2a245ab53e131cbf
Scatmandu <mcclur05@nsuok.edu>
2022-01-18 15:13:32 -0600

    added file 4

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = process.argv[2];
+let count = 0;
+
+request(URL, function (_error, _response, body) {
+  const bodyJSON = JSON.parse(body).results;
+
+  for (let i = 0; i < bodyJSON.length; i++) {
+    const characters = bodyJSON[i].characters;
+
+    for (let j = 0; j < characters.length; j++) {
+      const char = characters[j];
+      const charID = char.split('/')[5];
+
+      if (charID === '18') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
